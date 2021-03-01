6b8e54f717c74b72ebeb3c3fd7d4252864e5840f
Lena Zhang <64572479+leyulunna@users.noreply.github.com>
2021-03-01 15:37:36 +0800

    Refactoring and test

--- training/2021-02-26/calculator.ts
+++ training/2021-02-26/calculator.ts
@@ -1,21 +1,33 @@
-function myCalculator(num1: number, num2: number, type: string) {
+// Tests
+console.log(myCalculator(1, 2, "+") === 3);
+console.log(myCalculator(4, 8, "-") === -4);
+console.log(myCalculator(-1, -2, "*") === 2);
+console.log(myCalculator(9, -3, "/") === -3);
+console.log(isNaN(myCalculator("30", 0, "+")));
+console.log(isNaN(myCalculator(20, "1", "/")));
+console.log(isNaN(myCalculator(0, 0, "/")));
+
+function myCalculator(num1, num2, type) {
+  // Base cases
+  if (typeof num1 === "string") {
+    return NaN;
+  }
+  if (typeof num2 === "string") {
+    return NaN;
+  }
+  if (type === "/" && num2 === 0) {
+    return NaN;
+  }
+
   switch (type) {
     case "+":
-      var result = num1 + num2;
-      console.log(result);
-      return result;
+      return num1 + num2;
     case "-":
-      var result = num1 - num2;
-      console.log(result);
-      return result;
+      return num1 - num2;
     case "*":
-      var result = num1 * num2;
-      console.log(result);
-      return result;
+      return num1 * num2;
     case "/":
-      var result = num1 / num2;
-      console.log(result);
-      return result;
+      return num1 / num2;
     default:
       console.log("Sorry, it couldn't be computed by this calculator :(");
       return NaN;
--- training/2021-02-26/prime.ts
+++ training/2021-02-26/prime.ts
@@ -1,36 +1,29 @@
-// Note: Avoid deep nesting
-// 1. Function Definition (Input, Output)
-// 2. Base case (Validate user inputs or quick determination)
-// 3. Main Logic
-// 4. Return
+// Tests
+console.log(isPrime(-30) === false);
+console.log(isPrime(1) === false);
+console.log(isPrime(2) === true);
+console.log(isPrime(3) === true);
+console.log(isPrime(5) === true);
+console.log(isPrime(11) === true);
+console.log(isPrime(49) === false);
+console.log(isPrime(97) === true);
+console.log(isPrime(2) === true);
 
 function isPrime(p) {
+  // Base Cases
   if (p <= 1) {
     return false;
   }
   if (p === 2) {
     return true;
   }
-  // Handle all even cases.
   if (p % 2 === 0) {
     return false;
   }
-  for (let b = 3; b <= Math.sqrt(p); b++) {
+
+  for (let b = 2; b <= Math.sqrt(p); b++) {
     if (p % b === 0) return false;
   }
+
   return true;
 }
-
-// 1. Upload folder
-// 3. Why 5 works?
-
-// debugger;
-console.log(isPrime(-30) === false);
-console.log(isPrime(1) === false);
-console.log(isPrime(2) === true);
-console.log(isPrime(3) === true);
-console.log(isPrime(5) === true); // Why it works?
-console.log(isPrime(11) === true);
-console.log(isPrime(49) === false);
-console.log(isPrime(97) === true);
-console.log(isPrime(2) === true);
